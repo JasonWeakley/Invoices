38726cfdd74761339f9c1c9525430b2c008a5175
Jason Weakley <jason.b.weakley@gmail.com>
2016-04-25 16:01:20 -0500

    initial commit

---
+++ SQLQuery1.sql
@@ -0,0 +1,10 @@
+﻿SELECT
+ p.IdProduct,
+ p.Description,
+ p.Name AS "Product Name",
+ p.Price,
+ p.IdProductType,
+ pt.Name AS "Product Type Name"
+FROM Product p
+INNER JOIN ProductType pt
+  ON p.IdProductType = pt.IdProductType;
---
+++ dbo.Product.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Product] (
+    [IdProduct]     INT           NOT NULL,
+    [Name]          VARCHAR (55)  NOT NULL,
+    [Description]   VARCHAR (255) NOT NULL,
+    [Price]         REAL          NOT NULL,
+    [IdProductType] INT           NOT NULL,
+    [Quantity] INT NOT NULL, 
+    [Dimensions] INT NOT NULL, 
+    [Color] TEXT NOT NULL, 
+    PRIMARY KEY CLUSTERED ([IdProduct] ASC),
+    CONSTRAINT [FK_Product_ProductType] FOREIGN KEY ([IdProductType]) REFERENCES [dbo].[ProductType] ([IdProductType])
+);
+
